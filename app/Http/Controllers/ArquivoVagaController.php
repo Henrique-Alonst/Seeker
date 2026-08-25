a922cb6a00ff76a3459afd1567f0b2235d457c45
Henrique-Alonst <henriquealonso407@gmail.com>
2026-08-25 19:24:59 -0300

    Backend de dicas criado.

--- app/Http/Controllers/ArquivoVagaController.php
+++ app/Http/Controllers/ArquivoVagaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\ArquivoVaga;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 
 class ArquivoVagaController extends Controller
 {
